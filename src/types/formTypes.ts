9d43f64f47dbef00392e16814780e7305ac529ef
athira <athiraomanakuttan20@gmail.com>
2025-06-18 17:27:06 +0530

    feat(staff registration): staff registration form created for staffs

--- src/types/formTypes.ts
+++ src/types/formTypes.ts
@@ -2,3 +2,16 @@ export interface LoginType{
     userName : string,
     password : string
 }
+
+export interface StaffType {
+  name: string;
+  phoneNumber: string;
+  password: string;
+  designation: string;
+  email ?: string;
+  accessibleUsers ?: string;
+  staffImage ?: File | null;
+  openingBalance ?: string;
+  accessOfficialWhatsapp ?: boolean;
+  accessPhoneCallLog ?: boolean; 
+}
